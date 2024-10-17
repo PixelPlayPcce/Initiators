132343bfcd88a23cab2fc36b93c86061ae55d9b4
Aaditya Salgaonkar <aadityasalgaonkar@gmail.com>
2024-10-17 14:22:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    document.getElementById('form-status').innerText = 'Thanks, ' + name + '! We will get back to you soon.';
+});
+const burger = document.querySelector('.burger-menu');
+const navLinks = document.querySelector('.nav-links');
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+});
